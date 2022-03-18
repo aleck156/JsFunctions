a907747e553007a3a1b87aad75ae19b865c17fa2
Aleksander Jaworski <aleck156@gmail.com>
2022-03-18 06:12:18 +0100

    display prompt and assign results to poll answers

--- challenge1.js
+++ challenge1.js
@@ -5,12 +5,36 @@
 
 */
 
+const displayPrompt = function () {
+  const userInput = prompt(`What is your favourite programming language?
+  0: JavaScript
+  1: Python
+  2: Rust
+  3: C++
+  (Write option number): `);
+  return Number(userInput);
+};
+
+const updateAnswers = function (num) {
+  this.answers ? this.answers[num]++ : (this.answers[num] = 1);
+};
+
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const userInput = displayPrompt();
+    updateAnswersPoll(userInput);
+    console.log(this.answers);
+  },
 };
 
 const testData1 = [5, 2, 3];
 const testData2 = [1, 5, 3, 9, 6, 1];
+
+// poll.registerNewAnswer();
+const updateAnswersPoll = updateAnswers.bind(poll);
+poll.registerNewAnswer();
